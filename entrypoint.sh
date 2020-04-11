2057e42f0e2d7f6e3fd3e7889a9e4942a619d2ac
ı ɑɷ ɖεɾεƙ <quanmn.libra@gmail.com>
2020-04-11 18:57:38 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 set -eu
 
-if [ $WEBHOOK_DATA_TYPE == 'JSON' ] 
-then 
-    curl -X POST -H "Content-Type: application/json" -d "$data" $WEBHOOK_URL
-else
-    curl -X POST -H "Content-Type: application/json" --data "{ \"data\": \"$data\" }" $WEBHOOK_URL
-fi 
+curl -X POST -H "Content-Type: application/json" -d $data $WEBHOOK_URL
