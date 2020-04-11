bffc991965c3a8c4ffbf7de3a119540bdb26a3bf
ı ɑɷ ɖεɾεƙ <quanmn.libra@gmail.com>
2020-04-11 17:26:23 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -eu
 
-curl -X POST -H "Content-Type: application/json" --data "{ \"data\": \"$data\" }" $WEBHOOK_URL
+case "$DATA_TYPE" in 
+    #case 1 
+    "JSON") curl -X POST -H "Content-Type: application/json" --data $data $WEBHOOK_URL
+  
+    #case 3 
+    "") curl -X POST -H "Content-Type: application/json" --data "{ \"data\": \"$data\" }" $WEBHOOK_URL
+esac 
